1d012908bff86cd0070374364a89c93dfb18b48c
Dani Andhika Permana <daniandhika03@gmail.com>
2023-02-16 13:14:33 +0700

    Updated login padding

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -7,36 +7,41 @@
         </div>
         <div class="card border-0 rounded shadow">
           <div class="card-body">
-            <h4>LOGIN</h4>
+            <h4>Login</h4>
             <hr />
             <form @submit.prevent="login">
-              <div class="form-group">
-                <label>Email address</label>
-                <input
-                  type="email"
-                  v-model="user.email"
-                  class="form-control"
-                  placeholder="Email Address"
-                />
-              </div>
-              <div v-if="validation.email" class="mt-2 alert alert-danger">
-                {{ validation.email[0] }}
-              </div>
-
-              <div class="form-group">
-                <label>Password</label>
-                <input
-                  type="password"
-                  v-model="user.password"
-                  class="form-control"
-                  placeholder="Password"
-                />
+              <div class="row gy-3 mb-3">
+                <div class="col-12">
+                  <div class="form-group">
+                    <label class="mb-1">Email address</label>
+                    <input
+                      type="email"
+                      v-model="user.email"
+                      class="form-control"
+                      placeholder="Email Address"
+                    />
+                  </div>
+                  <div v-if="validation.email" class="mt-2 alert alert-danger">
+                    {{ validation.email[0] }}
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <label class="mb-1">Password</label>
+                    <input
+                      type="password"
+                      v-model="user.password"
+                      class="form-control"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
               </div>
               <div v-if="validation.password" class="mt-2 alert alert-danger">
                 {{ validation.password[0] }}
               </div>
               <button type="submit" class="btn btn-primary btn-block">
-                LOGIN
+                Login
               </button>
             </form>
           </div>
